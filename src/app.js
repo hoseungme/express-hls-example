483195f8fe0b0e475a1eea76ca741fef4851e525
HoseungJang <hsjang.dev@gmail.com>
2020-05-06 09:53:19 +0900

    ADD: express app

---
+++ src/app.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+const fs = require('fs');
+const hls = require('hls-server');
+
+app.get('/', (req, res) => {
+    return res.status(200).sendFile(`${__dirname}/client.html`);
+});
+
+const server = app.listen(3000);
+
+new hls(server, {
+    provider: {
+        exists: (req, cb) => {
+            const ext = req.url.split('.').pop();
+
+            if (ext !== 'm3u8' && ext !== 'ts') {
+                return cb(null, true);
+            }
+
+            fs.access(__dirname + req.url, fs.constants.F_OK, function (err) {
+                if (err) {
+                    console.log('File not exist');
+                }
+                cb(null, true);
+            });
+        },
+        getManifestStream: (req, cb) => {
+            const stream = fs.createReadStream(__dirname + req.url);
+            cb(null, stream);
+        },
+        getSegmentStream: (req, cb) => {
+            const stream = fs.createReadStream(__dirname + req.url);
+            cb(null, stream);
+        }
+    }
+});
