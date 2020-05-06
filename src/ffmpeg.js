f854a4f80568daebed8ac6539f816627a6bef4ba
HoseungJang <hsjang.dev@gmail.com>
2020-05-06 09:54:37 +0900

    ADD: ffmpeg

---
+++ src/ffmpeg.js
@@ -0,0 +1,15 @@
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+ffmpeg('videos/video.mp4', { timeout: 432000 }).addOptions([
+    '-profile:v baseline',
+    '-level 3.0',
+    '-start_number 0',
+    '-hls_time 10',
+    '-hls_list_size 0',
+    '-f hls'
+]).output('videos/output.m3u8').on('end', () => {
+    console.log('end');
+}).run();
